0b1f6f747a8b832385f4d84e2df6e7fc464192c8
GeorgeNonis <georgenonis@gmail.com>
2023-06-21 21:17:54 +0200

    done with the eyedrop

--- src/components/eyedrop/eyedrop.tsx
+++ src/components/eyedrop/eyedrop.tsx
@@ -7,14 +7,16 @@ const Eyedropper = ({ hexToRGB }: EyeDropProps) => {
   const { open, close, isSupported } = useEyeDropper();
   const [color, setColor] = useState("#fff");
   const [error, setError] = useState();
+  const [state, setState] = useState(false);
   // useEyeDropper will reject/cleanup the open() promise on unmount,
   // so setState never fires when the component is unmounted.
   const pickColor = () => {
+    setState(true);
     open()
       .then((color) => {
-        console.log({ color });
         hexToRGB(color.sRGBHex);
         setColor(color.sRGBHex);
+        setState(false);
       })
 
       .catch((e) => {
@@ -27,7 +29,12 @@ const Eyedropper = ({ hexToRGB }: EyeDropProps) => {
       {isSupported() ? (
         <BsEyedropper
           onClick={pickColor}
-          style={{ cursor: "pointer", width: "1.5rem", height: "1.5rem" }}
+          style={{
+            cursor: "pointer",
+            width: "1.5rem",
+            height: "1.5rem",
+            color: `${state ? "blue" : "unset"}`,
+          }}
         />
       ) : (
         <span>EyeDropper API not supported in this browser</span>
